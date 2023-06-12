8aa67f933e85c9963c25cf5f79f637b6efa64f44
Juewei Dong <juewei.dong@brainco.tech>
2023-06-12 12:21:25 -0400

    chore: bump up version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="clenv",
-    version="0.0.7",
+    version="0.0.8",
     description="ClearML config profile manager",
     long_description=long_description,
     long_description_content_type="text/markdown",
